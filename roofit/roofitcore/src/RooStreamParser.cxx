6a6e03bcab56dd55a6f4bcee4e3818f3c6015cf5
Debian Science Maintainers <debian-science-maintainers@lists.alioth.debian.org>
2012-07-14 12:56:35 +0000

    fix-typo-preceed-preced
    Gbp-Pq: Name 23-fix-typo-preceed-preced.patch

--- roofit/roofitcore/src/RooStreamParser.cxx
+++ roofit/roofitcore/src/RooStreamParser.cxx
@@ -204,7 +204,7 @@ TString RooStreamParser::readToken()
     }
 
     if (!quotedString) {
-      // Decide if next char is punctuation (exempt - and . that are part of floating point numbers, or +/- preceeding INF)
+      // Decide if next char is punctuation (exempt - and . that are part of floating point numbers, or +/- preceding INF)
       if (isPunctChar(c) && !(c=='.' && (isdigit(cnext)||isdigit(cprev))) 
 	  && !((c=='-'||c=='+') && (isdigit(cnext)||cnext=='.'||cnext=='i'||cnext=='I'))) {
 	if (first) {
